d3cfb8e290fa3a7129257e19a1d4acc307bbfcac
Emerson-Nunes <emerson_nunes2@hotmail.com>
2026-04-20 17:36:34 -0300

    Post service and Post Component finished

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   imports: [],
   templateUrl: './post.component.html',
-  styleUrl: './post.component.scss'
+  styleUrl: './post.component.scss',
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+  @Input() post!: { title: string; body: string };
+  randomPhotoUrl!: string;
 
+  ngOnInit(): void {
+    this.generateRandomPhoto();
+  }
+
+  private generateRandomPhoto(): void {
+    const randomSeed = this.generateRandomNumber(1000);
+  }
+
+  private generateRandomNumber(max: number): number {
+    return Math.floor(Math.random() * max);
+  }
 }
